c80b3c16a2f4c0e3e366a4d8cef152d37e823eea
Micah Clarke <micahclarke03@gmail.com>
2024-11-02 17:51:31 -0500

    Testing out moving some logic around

--- app.py
+++ app.py
@@ -21,32 +21,22 @@ SCOPE = "openid email profile"
 oauth2 = OAuth2Component(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, AUTHORIZATION_URL, TOKEN_URL, TOKEN_URL, REVOKE_URL)
 
 
-# Initialize a rerun toggle in session state
-if "rerun_trigger" not in st.session_state:
-    st.session_state["rerun_trigger"] = False
 
 # OAuth flow
-if 'token' not in st.session_state:
-    result = oauth2.authorize_button(
-        "Continue with Google",
-        REDIRECT_URI,
-        SCOPE,
-        icon="data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 48 48'%3E%3Cdefs%3E%3Cpath id='a' d='M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z'/%3E%3C/defs%3E%3CclipPath id='b'%3E%3Cuse xlink:href='%23a' overflow='visible'/%3E%3C/clipPath%3E%3Cpath clip-path='url(%23b)' fill='%23FBBC05' d='M0 37V11l17 13z'/%3E%3Cpath clip-path='url(%23b)' fill='%23EA4335' d='M0 11l17 13 7-6.1L48 14V0H0z'/%3E%3Cpath clip-path='url(%23b)' fill='%2334A853' d='M0 37l30-23 7.9 1L48 0v48H0z'/%3E%3Cpath clip-path='url(%23b)' fill='%234285F4' d='M48 48L17 24l-4-3 35-10z'/%3E%3C/svg%3E"
-    )
+result = oauth2.authorize_button(
+    "Continue with Google",
+    REDIRECT_URI,
+    SCOPE,
+    icon="data:image/svg+xml;charset=utf-8,%3Csvg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' viewBox='0 0 48 48'%3E%3Cdefs%3E%3Cpath id='a' d='M44.5 20H24v8.5h11.8C34.7 33.9 30.1 37 24 37c-7.2 0-13-5.8-13-13s5.8-13 13-13c3.1 0 5.9 1.1 8.1 2.9l6.4-6.4C34.6 4.1 29.6 2 24 2 11.8 2 2 11.8 2 24s9.8 22 22 22c11 0 21-8 21-22 0-1.3-.2-2.7-.5-4z'/%3E%3C/defs%3E%3CclipPath id='b'%3E%3Cuse xlink:href='%23a' overflow='visible'/%3E%3C/clipPath%3E%3Cpath clip-path='url(%23b)' fill='%23FBBC05' d='M0 37V11l17 13z'/%3E%3Cpath clip-path='url(%23b)' fill='%23EA4335' d='M0 11l17 13 7-6.1L48 14V0H0z'/%3E%3Cpath clip-path='url(%23b)' fill='%2334A853' d='M0 37l30-23 7.9 1L48 0v48H0z'/%3E%3Cpath clip-path='url(%23b)' fill='%234285F4' d='M48 48L17 24l-4-3 35-10z'/%3E%3C/svg%3E"
+)
     
-    # If login was successful
-    if result:
-        # Store the token in session state
-        st.session_state['token'] = result.get('token')
-        
-        # Clear query params to avoid repeating the OAuth flow
-        st.experimental_set_query_params()
-        
-        # Toggle the rerun trigger to force a page refresh
-        st.session_state["rerun_trigger"] = not st.session_state["rerun_trigger"]
-        
-else:
+# If login was successful
+if result:
     # Display user content when logged in
     st.success("You are logged in!")
     st.write("Token:", st.session_state.token)
+        
+else:
+    st.success("You are not logged in!")
+
 
